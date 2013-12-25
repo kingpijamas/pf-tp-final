ee7356034712ce1d18cd8f02dabf4d5e37dfb1cb
kpjjpk <juanpablorey.10@gmail.com>
2013-12-24 22:11:54 -0300

    (Dirty commit: compilation untested) Movement module done.

--- src/Area.elm
+++ src/Area.elm
@@ -1,9 +1,10 @@
 module Area where
-import src/Matrix.elm as Matrix
+import src/utils/Matrix.elm as M
+import src/CardinalDirs.elm as C
 
-type Area a = Matrix.Matrix a
+type Area a = M.Matrix a
 
-type Coords = Matrix.Position
+type Coords = M.Position
 
 coords = position
 
@@ -13,12 +14,22 @@ getX loc = loc.row
 getY:Coords->Int
 getY loc = loc.col
 
-type Location = { area:Area
-				, coords:Coords
-				}
+(+):Coords->(Either Coords C.CardinalDirs)->Maybe(Coords)
+(+) a b = let
+             b' = case b of
+                       Left coord   ->  coord
+                       Right dir    ->  asCoords dir
+          in
+             case b' of
+                Nothing     ->  Nothing
+                Just coord  ->  coords ((getX a)+(getX b)) ((getY a)+(getY b))
 
-type Locatable a = { a | location:Location }
+asCoords:C.CardinalDirs->Maybe(Coords)
+asCoords dir = case dir of
+                    North   ->  Just (coords (-1) 0)
+                    East    ->  Just (coords 0 1)
+                    South   ->  Just (coords 1 0)
+                    West    ->  Just (coords 0 (-1))
+                    _       ->  Nothing
 
-{--TODO move--}
-{--TODO add--}
-{--TODO remove?--}
+type Locatable a = { a | area:Area a, location:Coords }
--- src/Movement.elm
+++ src/Movement.elm
@@ -1,25 +1,31 @@
 module Movement where
-import src/Actor.elm
-import src/Area.elm
-import src/CardinalDirs.elm
+import Automaton
+import Area
+import CardinalDirs
 
-type Moving a = Actor a Maybe(Area)
-{--thus, do:self->Maybe(Area)--}
+type Motor a = Automaton (Locatable a,CardinalDirs) Maybe(Area)
 
+inDir:((Locatable a)->Coords->Maybe(Area a))->(Locatable a,CardinalDirs)->Maybe(Area a)
+inDir f (loc,dir) = let proposed = from+dir
+                    in 
+                      case proposed of
+                        Nothing -> Nothing
+                        Just to -> f a to
 
-{--un motor es un automata. La funcion con la que se crea el motor es la estrategia/el tipo de movimiento--}
-{--Ahora bien, viendolo mejor, por no pensar el motor, solo actuar de forma reactiva, 
---seria SOLO el TIPO de movimiento la funcion, y NO la estrategia--}
+mv:(Locatable a)->Coords->Maybe(Area a)
+mv toMv newPos = let whr = toMv.area
+                     from = toMv.location
+                 in
+                   case whr `get` newPos
+                       Just sth -> Nothing
+                       Nothing -> add (whr `remove` toMv) (toMv | location <- newPos) newPos 
 
+motor:Motor a
+motor = pure.(inDir mv)
 
+type Moveable a = { a | motor:(Motor a) }
 
-
-type Motor a = Actor (Locatable a) Maybe(Area)
-
-asCoords:CardinalDirs->Maybe(Coords)
-asCoords dir = case dir of
-                    North   ->  Just coords (-1) 0
-                    East    ->  Just coords 0 1
-                    South   ->  Just coords 1 0
-                    West    ->  Just coords 0 (-1)
-                    _       ->  Nothing
+--Draft para hacer esto:
+---un motor es un automata. La funcion con la que se crea el motor es la estrategia/el tipo de movimiento-
+--{--Ahora bien, viendolo mejor, por no pensar el motor, solo actuar de forma reactiva, 
+----seria SOLO el TIPO de movimiento la funcion, y NO la estrategia--}
